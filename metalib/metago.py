5882d0af984b7054a5c831b523d1ffad14eeff14
Ilies <79206019+IliesElJ@users.noreply.github.com>
2026-01-24 20:27:22 +0000

    lint

--- metalib/metago.py
+++ metalib/metago.py
@@ -10,40 +10,51 @@ from metalib.metastrategy import MetaStrategy
 
 
 class MetaGO(MetaStrategy):
-    def __init__(self, symbols, timeframe, tag, active_hours, risk_factor=1, lookahead=24, hist_length=10000,):
+    def __init__(
+        self,
+        symbols,
+        timeframe,
+        tag,
+        active_hours,
+        risk_factor=1,
+        lookahead=24,
+        hist_length=10000,
+    ):
         super().__init__(symbols, timeframe, tag, active_hours)
-        self.indicators         = None
-        self.quantile           = None
-        self.model              = None
-        self.indicators_std     = None
-        self.indicators_mean    = None
-        self.state              = None
-        self.risk_factor        = risk_factor
-        self.lookahead          = lookahead
-        self.hist_length        = hist_length
-        self.telegram           = True
-        self.logger             = logging.getLogger(__name__)
-        logging.basicConfig(filename=f'../logs/{self.tag}.log', encoding='utf-8', level=logging.DEBUG)
+        self.indicators = None
+        self.quantile = None
+        self.model = None
+        self.indicators_std = None
+        self.indicators_mean = None
+        self.state = None
+        self.risk_factor = risk_factor
+        self.lookahead = lookahead
+        self.hist_length = hist_length
+        self.telegram = True
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(
+            filename=f"../logs/{self.tag}.log", encoding="utf-8", level=logging.DEBUG
+        )
 
     def signals(self):
-        ohlc        = self.data[self.symbols[0]]
-        indicators  = self.retrieve_indicators(ohlc)
-        close       = ohlc['close']
+        ohlc = self.data[self.symbols[0]]
+        indicators = self.retrieve_indicators(ohlc)
+        close = ohlc["close"]
 
         self.indicators = indicators
 
         # Demean Indicators
         indicators = indicators.tail(3)
-        close      = close.tail(3)
+        close = close.tail(3)
         indicators = (indicators - self.indicators_mean) / self.indicators_std
 
-        uptrend     = indicators['uptrend']
-        downtrend   = indicators['downtrend']
-        true_open_yearly = indicators['true_open_monthly']
+        uptrend = indicators["uptrend"]
+        downtrend = indicators["downtrend"]
+        true_open_yearly = indicators["true_open_monthly"]
 
-        mask_uptrend_below_yearly   = ( uptrend > 0 ) & (close < true_open_yearly)
-        mask_downtrend_above_yearly = ( downtrend > 0 ) & (close > true_open_yearly)
-        mask_combined               = mask_uptrend_below_yearly | mask_downtrend_above_yearly
+        mask_uptrend_below_yearly = (uptrend > 0) & (close < true_open_yearly)
+        mask_downtrend_above_yearly = (downtrend > 0) & (close > true_open_yearly)
+        mask_combined = mask_uptrend_below_yearly | mask_downtrend_above_yearly
 
         y_hat = self.model.predict_proba(indicators)[:, 1]
         vote = mask_combined.iloc[-1]
@@ -59,30 +70,36 @@ class MetaGO(MetaStrategy):
         else:
             self.state = 0
 
-        print(f"{self.tag}::: Open positions for strategy: {self.tag}: {self.are_positions_with_tag_open()}")
-        print(f"{self.tag}::: Vote of indicators: {vote}, and last 3 predicted probabilities: {y_hat}")
+        print(
+            f"{self.tag}::: Open positions for strategy: {self.tag}: {self.are_positions_with_tag_open()}"
+        )
+        print(
+            f"{self.tag}::: Vote of indicators: {vote}, and last 3 predicted probabilities: {y_hat}"
+        )
 
         signal_line = indicators.iloc[[-1]]
-        signal_line.loc[:, 'vote'] = vote
-        signal_line.loc[:, 'predicted_proba'] = y_hat[-1]
+        signal_line.loc[:, "vote"] = vote
+        signal_line.loc[:, "predicted_proba"] = y_hat[-1]
 
         self.signals_data = signal_line
 
     def check_conditions(self):
         mean_entry_price, num_positions = self.get_positions_info()
-        mean_entry_price                = round(mean_entry_price, 4)
+        mean_entry_price = round(mean_entry_price, 4)
         if self.state == 0:
             pass
         elif self.state == 1:
             self.execute(symbol=self.symbols[0], short=False)
             # Send a message when an order is entered
             self.send_telegram_message(
-                f"Entered BUY order for {self.symbols[0]} with volume: {self.size_position} et pelo sa achete! Mean Entry Price: {mean_entry_price}, Number of Positions: {num_positions}")
+                f"Entered BUY order for {self.symbols[0]} with volume: {self.size_position} et pelo sa achete! Mean Entry Price: {mean_entry_price}, Number of Positions: {num_positions}"
+            )
         elif self.state == -1:
             self.execute(symbol=self.symbols[0], short=True)
             # Send a message when an order is entered
             self.send_telegram_message(
-                f"Entered SELL order for {self.symbols[0]} with volume: {self.size_position} et pelo ca vend: Mean Entry Price: {mean_entry_price}, Number of Positions: {num_positions}")
+                f"Entered SELL order for {self.symbols[0]} with volume: {self.size_position} et pelo ca vend: Mean Entry Price: {mean_entry_price}, Number of Positions: {num_positions}"
+            )
         elif self.state == -2:
             self.close_all_positions()
             # Send a message when positions are closed
@@ -90,15 +107,17 @@ class MetaGO(MetaStrategy):
 
     def fit(self, data=None):
         # Define the UTC timezone
-        utc = pytz.timezone('UTC')
+        utc = pytz.timezone("UTC")
         # Get the current time in UTC
-        end_time    = datetime.now(utc)
-        start_time  = end_time - timedelta(days=60)
+        end_time = datetime.now(utc)
+        start_time = end_time - timedelta(days=60)
         # Set the time components to 0 (midnight) and maintain the timezone
-        end_time    = end_time.replace(hour=0, minute=0, second=0, microsecond=0).astimezone(utc)
-        start_time  = start_time.astimezone(utc)
+        end_time = end_time.replace(
+            hour=0, minute=0, second=0, microsecond=0
+        ).astimezone(utc)
+        start_time = start_time.astimezone(utc)
         # Grab lookahead parameter
-        lookahead   = self.lookahead
+        lookahead = self.lookahead
         hist_length = self.hist_length
 
         # Pulling last days of data
@@ -108,39 +127,47 @@ class MetaGO(MetaStrategy):
         else:
             data = data[self.symbols[0]]
 
-        returns     = data.loc[:, 'close'].apply(np.log) - data.loc[:, 'open'].apply(np.log)
+        returns = data.loc[:, "close"].apply(np.log) - data.loc[:, "open"].apply(np.log)
 
         # Compute rolling next returns series
         T = returns.shape[0]
-        next_returns    = [np.sum(returns[i + 1: i + lookahead]) for i in range(T)]
-        next_returns    = pd.Series(next_returns, index=returns.index)
+        next_returns = [np.sum(returns[i + 1 : i + lookahead]) for i in range(T)]
+        next_returns = pd.Series(next_returns, index=returns.index)
 
         # Indicators
-        indicators      = self.retrieve_indicators(ohlc_df=data).dropna()
-        close           = data['close']
+        indicators = self.retrieve_indicators(ohlc_df=data).dropna()
+        close = data["close"]
 
         # Retrieve history
-        hist_indicators     = indicators[:hist_length]
-        hist_next_returns   = next_returns.loc[hist_indicators.index]
+        hist_indicators = indicators[:hist_length]
+        hist_next_returns = next_returns.loc[hist_indicators.index]
 
         # Demean from history
-        indicators     = (hist_indicators - hist_indicators.mean()) / hist_indicators.std()
-        next_returns   = (hist_next_returns - hist_next_returns.mean()) / hist_next_returns.std()
+        indicators = (hist_indicators - hist_indicators.mean()) / hist_indicators.std()
+        next_returns = (
+            hist_next_returns - hist_next_returns.mean()
+        ) / hist_next_returns.std()
 
         print(f"Before filtering: {indicators.shape[0]}")
         # Mask for uptrend and close below true yearly open
-        uptrend = indicators['uptrend']
-        downtrend = indicators['downtrend']
-        true_open_monthly = indicators['true_open_monthly']
+        uptrend = indicators["uptrend"]
+        downtrend = indicators["downtrend"]
+        true_open_monthly = indicators["true_open_monthly"]
 
         # Ensure all series are aligned to the same index
-        aligned_close, aligned_true_open_monthly = close.align(true_open_monthly, join='inner')
-        aligned_uptrend, aligned_close = uptrend.align(aligned_close, join='inner')
-        align_downtrend, aligned_close = downtrend.align(aligned_close, join='inner')
+        aligned_close, aligned_true_open_monthly = close.align(
+            true_open_monthly, join="inner"
+        )
+        aligned_uptrend, aligned_close = uptrend.align(aligned_close, join="inner")
+        align_downtrend, aligned_close = downtrend.align(aligned_close, join="inner")
 
         # Perform the comparison with aligned indices
-        mask_uptrend_below_yearly = ( aligned_uptrend > 0 ) & (aligned_close < aligned_true_open_monthly)
-        mask_downtrend_above_yearly = ( align_downtrend > 0 ) & (aligned_close > aligned_true_open_monthly)
+        mask_uptrend_below_yearly = (aligned_uptrend > 0) & (
+            aligned_close < aligned_true_open_monthly
+        )
+        mask_downtrend_above_yearly = (align_downtrend > 0) & (
+            aligned_close > aligned_true_open_monthly
+        )
 
         # Combine masks with all other times as False
         mask_combined = mask_uptrend_below_yearly | mask_downtrend_above_yearly
@@ -153,10 +180,14 @@ class MetaGO(MetaStrategy):
 
         # Fit DecisionTreeClassifier
         tree_dummy = DecisionTreeClassifier(max_depth=5).fit(X, y)
-        print(f"{self.tag}::: DecisionTree Model trained from {X.index[0]} to {X.index[-1]}.")
+        print(
+            f"{self.tag}::: DecisionTree Model trained from {X.index[0]} to {X.index[-1]}."
+        )
 
         # Add line to log file to signal training completion
-        self.logger.info(f"DecisionTree Model trained from {X.index[0]} to {X.index[-1]}.")
+        self.logger.info(
+            f"DecisionTree Model trained from {X.index[0]} to {X.index[-1]}."
+        )
 
         # Save model, 1st and 2nd indicator moments
         self.model = tree_dummy
@@ -173,53 +204,68 @@ class MetaGO(MetaStrategy):
 
     def retrieve_indicators(self, ohlc_df):
         ohlc = ohlc_df.copy()
-        ohlc_daily = ohlc.resample('1D').agg({'open': 'first',
-                                              'high': 'max',
-                                              'low': 'min',
-                                              'close': 'last',
-                                              })
-        closes  = ohlc.loc[:, 'close']
+        ohlc_daily = ohlc.resample("1D").agg(
+            {
+                "open": "first",
+                "high": "max",
+                "low": "min",
+                "close": "last",
+            }
+        )
+        closes = ohlc.loc[:, "close"]
 
-        short_sma   = closes.rolling(12).median()
-        long_sma    = closes.rolling(24).median()
+        short_sma = closes.rolling(12).median()
+        long_sma = closes.rolling(24).median()
 
-        uptrend     = (closes > short_sma) & (short_sma > long_sma)
-        downtrend   = (closes < short_sma) & (short_sma < long_sma)
-        sideways    = ~(uptrend | downtrend)
+        uptrend = (closes > short_sma) & (short_sma > long_sma)
+        downtrend = (closes < short_sma) & (short_sma < long_sma)
+        sideways = ~(uptrend | downtrend)
         print(f"{self.tag}::: Computed Trend indicators")
 
-        true_open_weekly    = get_last_monday_6pm_open_ffill(ohlc, ohlc.index)
-        true_open_monthly   = get_second_monday_open_ffill(ohlc_daily, ohlc.index)
+        true_open_weekly = get_last_monday_6pm_open_ffill(ohlc, ohlc.index)
+        true_open_monthly = get_second_monday_open_ffill(ohlc_daily, ohlc.index)
         print(f"{self.tag}::: Computed True opens (weekly, monthly) ")
 
         # Compute differences between various true open prices
-        crossed_diff_monthly_weekly     = true_open_monthly - true_open_weekly
-        above_true_open_monthly_diff    = closes - true_open_monthly
-        above_true_open_weekly_diff     = closes - true_open_weekly
+        crossed_diff_monthly_weekly = true_open_monthly - true_open_weekly
+        above_true_open_monthly_diff = closes - true_open_monthly
+        above_true_open_weekly_diff = closes - true_open_weekly
         print(f"{self.tag}::: Computed True opens diffs (weekly, monthly) ")
 
         # Merge Features
-        indicators =  [ uptrend, downtrend, sideways,
-                        true_open_weekly, true_open_monthly,
-                        crossed_diff_monthly_weekly,
-                        above_true_open_monthly_diff, above_true_open_weekly_diff,
-                        closes
-                        ]
+        indicators = [
+            uptrend,
+            downtrend,
+            sideways,
+            true_open_weekly,
+            true_open_monthly,
+            crossed_diff_monthly_weekly,
+            above_true_open_monthly_diff,
+            above_true_open_weekly_diff,
+            closes,
+        ]
 
-        indicators          = pd.concat(indicators, axis=1).iloc[1:]
-        indicators.columns  = [ 'uptrend', 'downtrend', 'sideways',
-                                'true_open_weekly', 'true_open_monthly',
-                                'crossed_diff_monthly_weekly',
-                                'above_true_open_monthly_diff', 'above_true_open_weekly_diff',
-                                'close']
-        indicators.index    = pd.to_datetime(indicators.index)
-        indicators.columns  = indicators.columns.astype(str)
+        indicators = pd.concat(indicators, axis=1).iloc[1:]
+        indicators.columns = [
+            "uptrend",
+            "downtrend",
+            "sideways",
+            "true_open_weekly",
+            "true_open_monthly",
+            "crossed_diff_monthly_weekly",
+            "above_true_open_monthly_diff",
+            "above_true_open_weekly_diff",
+            "close",
+        ]
+        indicators.index = pd.to_datetime(indicators.index)
+        indicators.columns = indicators.columns.astype(str)
         print(f"{self.tag}::: Merged indicators")
 
         return indicators
 
+
 def assign_cat(val):
-    if val < 0.:
+    if val < 0.0:
         return 0
     else:
         return 1
